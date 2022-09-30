ce533d11a8d39779f687698b507496b3bbc4203d
Satoru Takeuchi <satoru.takeuchi@gmail.com>
2022-09-30 23:48:08 +0000

    update
    Signed-off-by: Satoru Takeuchi <satoru.takeuchi@gmail.com>

--- 052-oom-killer/oom.go
+++ 052-oom-killer/oom.go
@@ -1,7 +1,12 @@
 package main
 
+import (
+	"fmt"
+)
+
 func main() {
 	 a := make([]byte, 8*1024*1024*1024)
+	 fmt.Println("MEMORY ALLOCATED")
 	 for i := 0; i < len(a); i += 4096 {
 		 a[i] = 1
 	 }
